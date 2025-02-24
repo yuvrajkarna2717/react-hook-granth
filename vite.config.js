ae97546947dff48abb1daf3ee9bcd0c2c791288c
yuvarjkarna2717 <173258726+yuvrajkarna2717@users.noreply.github.com>
2025-02-24 21:11:27 +0530

    added first custom hook useCounter

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/// vite.config.js
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.js",
+  },
+});
