82f6628b06b498879f9d89d2ece04a03f767e829
Yuvraj Karna <yuvrajkarna.code@gmail.com>
2025-04-20 10:14:37 +0530

    Add useOnlineStatus hook

---
+++ src/hooks/useOnlineStatus.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+/**
+ * Custom hook to track online/offline status.
+ * @returns {boolean} - `true` if online, `false` if offline.
+ */
+export default function useOnlineStatus() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const goOnline = () => setIsOnline(true);
+    const goOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", goOnline);
+    window.addEventListener("offline", goOffline);
+
+    // Clean up
+    return () => {
+      window.removeEventListener("online", goOnline);
+      window.removeEventListener("offline", goOffline);
+    };
+  }, []);
+
+  return isOnline;
+}
